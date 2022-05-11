c4ee54abc9d10756d284176f02d5e7004d1254a0
Владимир Бутеску <vbutescu2000@gmail.com>
2022-05-11 21:06:16 +0300

    fix

--- modules/andrich_maria_sort_facility/include/SortFacility.h
+++ modules/andrich_maria_sort_facility/include/SortFacility.h
@@ -6,13 +6,11 @@
 #include <string.h>
 
 class Sort {
-
-public:
-
-	int op_preced(const char c);
-	bool op_left_assoc(const char c);
-	unsigned int op_arg_count(const char c);
-	bool shunting_yard(const char* input, char* output);
+ public:
+int op_preced(const char c);
+bool op_left_assoc(const char c);
+unsigned int op_arg_count(const char c);
+bool shunting_yard(const char* input, char* output);
 };
 
 #endif  //  MODULES_ANDRICH_MARIA_SORT_FACILITY_INCLUDE_SORTFACILITY_H_
