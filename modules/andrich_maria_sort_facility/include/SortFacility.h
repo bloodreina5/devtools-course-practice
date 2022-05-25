0b42dbd8140d01381f1e39a60bcfdd25ed083ccd
bloodreina5 <65079069+bloodreina5@users.noreply.github.com>
2022-05-25 18:39:38 +0200

    11

--- modules/andrich_maria_sort_facility/include/SortFacility.h
+++ modules/andrich_maria_sort_facility/include/SortFacility.h
@@ -7,12 +7,13 @@
 
 class Sort {
 
-  public:
 
-    int op_preced(const char c1);
-  bool op_left_assoc(const char c1);
-  unsigned int op_arg_count(char c1);
-  bool shunting_yard(const char * input, char * output);
+public:
+
+	int op_preced(const char c1);
+	bool op_left_assoc(const char c1);
+	unsigned int op_arg_count(const char c1);
+	bool shunting_yard(const char * input, char * output);
 };
 
 #endif  //  MODULES_ANDRICH_MARIA_SORT_FACILITY_INCLUDE_SORTFACILITY_H_
